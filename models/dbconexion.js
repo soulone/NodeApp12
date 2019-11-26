2d61780108f1e33318b0de6b0db0f5733826727f
soulone <ascf24032@gmail.com>
2019-11-26 13:10:43 -0500

    First Commit

--- models/dbconexion.js
+++ models/dbconexion.js
@@ -2,8 +2,10 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
   host:"localhost",
   user:"root",
-  password:"",
-  database:"tienda"
+  password:"root",
+  database:"tienda",
+  port: 8889
+  
 });
 conn.connect();
 module.exports = conn;
